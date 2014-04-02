a85fe042b508e6fc3f0f3da6e44822170b6c3734
Syriamanal <Shadinachat123@gmail.com>
2014-04-02 10:55:21 -0700

    Uhh this all i under stand of the new api! XD

---
+++ src/xEcon/xEcon.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace PEMapModder\xEconPluin;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\Player;
+use pocketmine\utils\TextFormat as Colors;
+
+class MainPlugin extends PluginBase{
+    public function onCommand(CommandSender $issuer, Command $cmd, $label, array $args){
+
+}
+}
+
